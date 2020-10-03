c1b78754a3cbc84e94b99eb441a6199accce9e5d
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-10-03 21:33:57 +0700

    upadate

--- app/routes/tutorial.route.js
+++ app/routes/tutorial.route.js
@@ -1,13 +1,15 @@
-module.exports = app {
-    const tutorials = require ("../controllers/tutorial.controller")
+module.exports = (app) => {
+  const tutorials = require("../controllers/tutorial.controller");
 
-    var router = require('express').Router()
+  var router = require("express").Router();
 
-    router.post("/")
-    router.get("/")
-    router.get("/:id")
-    router.get("/published")
-    router.put("/:id")
-    router.delete("/:id")
-    router.delete("/")
-}
+  router.post("/", tutorials.create);
+  router.get("/", tutorials.findAll);
+  router.get("/:id", tutorials.findOne);
+  router.get("/published", tutorials.findAllPublished);
+  router.put("/:id", tutorials.update);
+  router.delete("/:id", tutorials.delete);
+  router.delete("/", tutorials.deleteAll);
+
+  app.use("/api/v1/tutorials", router);
+};
